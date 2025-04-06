ed7138e81bda56477f14485e52cbbabc224fe203
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-04-06 12:49:48 +0530

    ✨Add. Added the Jwt validator and spring security along with the exception handling

---
+++ src/main/java/com/rapidshine/carwash/bookingservice/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.rapidshine.carwash.bookingservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private  JwtValidator jwtValidator;
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Enforce stateless session
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("booking/health").permitAll()
+                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html","/swagger-resources" +
+                                "/**","/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                ;
+
+        http.addFilterBefore(jwtValidator, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+}
